3d587aeaaa0c2a87ec14ccf472b1d57edbbcea33
vadilkin <valeriadilkin@gmail.com>
2025-02-06 13:37:26 -0300

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./dist/index/**/*.{html,js,ts,jsx,tsx}", // Ajustado para Next.js 15
     "./src/app/**/*.{js,ts,jsx,tsx}", // Ajustado para Next.js 15
     "./src/components/**/*.{js,ts,jsx,tsx}", // Ajustado para componentes dentro de src
   ],
